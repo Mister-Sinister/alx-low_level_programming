8bf237d29c094649944162bf2096c35d0a0167bc
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-08 10:42:46 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
- * prints a phrase
- * returns 0 if exited properly
+ * main - prints "Programming is like a multilingual puzzle
+ * Return: 0 if exited properly
  */
 
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
