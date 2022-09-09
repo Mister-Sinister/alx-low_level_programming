bda82efa624658558a187600b70a7278b6bb9d68
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-09 12:49:56 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = (n % 10);
+	int last_digit;
 
+	last_digit = (n % 10);
 	printf("Last digit of %d is %d", n, (n % 10));
 	if (last_digit > 5)
 	{
