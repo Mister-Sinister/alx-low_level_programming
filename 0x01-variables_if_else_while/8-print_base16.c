da74b4e6ec797734a2ee3069c7d6884729f0e3e1
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-09 15:24:07 -0700

    Task 8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 0xff; n++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar((char) n);
+		putchar(n);
+	}
+	for (n = 97; n < 103; n++)
+	{
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
