4d2cc65c063fe98cf394d6787b3a63c380f7b5d4
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-15 16:00:20 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_square - prints a square of size
+ * @size: the size of square # to print
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; i++)
+		{
+			printf("#");
+		}
+	}
+}
