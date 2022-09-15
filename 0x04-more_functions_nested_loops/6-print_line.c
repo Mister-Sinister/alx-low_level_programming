263e5a9c3f63cbb7dff4aa9371fb756851836a78
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-15 13:19:42 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - prints the underscore character @n times if @n is greater
+ * than 0, followed by a new line. Otherwise, a new line.
+ * @n: is the number of times the underscore will be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
