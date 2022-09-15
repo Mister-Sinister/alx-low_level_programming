1e9600cd664e2d417a965399282372fb69a03554
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-15 15:48:30 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,17 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			int j;
+
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
-		_putchar('\n');
 	}
 	else
 	{
