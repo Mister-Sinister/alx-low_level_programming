2bd0f3bf232788c2d2fe693dc745d08d240bd421
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-15 17:23:59 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints a right sided triangle
+ * @size: is the size of the triangle to be printed
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j, k;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - 1; j > i; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = -1; k < i; k++)
+			{
+				_putchar("#");
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
