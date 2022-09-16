46eb9e12e51145925a9a4b70f4c4829e34130ca8
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-15 17:04:38 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - prints numbers from 1 - 100, Fizz for numbers
+ * divisible by 3, Buzz for numbers divisible by 5, FizzBuzz for
+ * numbers divisible by 3 and 5
+ * Return: Always 0
+ */
 
 int main(void)
 {
@@ -24,4 +30,5 @@ int main(void)
 		}
 	}
 	printf("\n");
+	return (0);
 }
