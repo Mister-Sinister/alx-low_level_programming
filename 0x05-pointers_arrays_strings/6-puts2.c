5c2872c1f6375213cb93f8bb21fb4ea7cafbb1aa
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 16:04:57 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other charcter of a string, followed
+ * by a newline
+ * @str: the string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (i != '\0')
+	{
+		if i
+		_putchar(s[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
