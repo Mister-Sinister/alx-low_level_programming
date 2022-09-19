d3e9aa2e5d959b21139ef25fc463125bdfcd533a
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 16:19:38 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,16 +4,21 @@
  * by a newline
  * @str: the string to be printed
  */
-
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	i = 0;
-	while (str[i] != '\0')
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
+
 	_putchar('\n');
 }
