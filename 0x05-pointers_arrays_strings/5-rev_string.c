1cbff6434bc04260d29100e81a4d21086a6653df
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 12:43:26 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	char *temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		temp[i] = s[i];
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		s[j] = temp[i - j];
+	}
+}
