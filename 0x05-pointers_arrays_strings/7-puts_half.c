c93dd3b6bcd7f60320ddebded3ca939e76795e0d
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 16:37:51 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of a string
+ * @str: the pointer of the string
+ */
+
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len; i++)
+		{
+			_puts(str[i]);
+		}
+	}
+	else
+	{
+		i = len / 2;
+		while (i < len - 1)
+		{
+			_puts(str[i]);
+			i++;
+		}
+	}
+}
