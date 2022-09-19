80fe53ea86ee382dce570adf1a016075467defe6
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 11:56:44 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the pointer to the string to reverse
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+
+	while (i <= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
