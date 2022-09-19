afb8ecfc26b17f2cc06cd65dc2aa79a6436650cf
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-19 10:33:08 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer n and changes what it points to to be 98
+ * @n: the pointer to be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
