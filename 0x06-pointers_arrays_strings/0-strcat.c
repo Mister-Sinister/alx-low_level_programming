9fb5ed27a6caf480e73883234f77b417e4af5952
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-21 12:06:40 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcat - appends one string to another
+ * @src: the string to append
+ * @dest: the string to be appended to
+ * Return: The concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(dest) + sizeof(src) - sizeof(char));
+
+	buffer = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (buffer);
+}
