b3d502f0c565e484d4885b28e4b777d767fe8873
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-21 12:25:41 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,6 +10,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char *buffer;
+	int i;
 
 	buffer = dest;
 
@@ -18,6 +19,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest++;
 	}
 
+	i = 0;
 	while (i < n && *src != '\0')
 	{
 		*dest = *src;
