851ba6bb353c0ecf0ff83ac25068ff16baadce82
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-22 15:21:43 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,31 +3,20 @@
  * _strcmp - compares two strings
  * @s1: the first string
  * @s2: the second string
- * Return: 0 if they are equal, 1 if s1 > s2 and -1 if s1 < s2
+ * Return: 0 if they are equal, greater than 0 if s1 > s2
+ * and -1 if s1 < s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int n, check;
-
-	check = 0;
-	n = 0;
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 == *s2)
 	{
-		n++;
-		if (*s1 == *s2)
+		if (*s1 == '\0')
 		{
-			check++;
+			return (0);
 		}
 		s1++;
 		s2++;
 	}
-	if (check == n)
-	{
-		return (0);
-	}
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (*s1 - *s2);
 }
