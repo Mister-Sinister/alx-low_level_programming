5aa17bf1670844459bea43b5dab3f57103404396
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-22 10:19:21 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the contents of an array
+ * @a: the pointer to the array
+ * @n: the number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+
+		n--;
+	}
+}
