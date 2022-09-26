b2756763b3099d0b5b479f3efdd6fd66a181d153
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-26 12:42:11 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - checks if a character exists in a string
+ * @s: the string
+ * @c: the character
+ * Return: c is the character is present, otherwise NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != NULL)
+	{
+		if (s[i] == c)
+			return (s[i]);
+	}
+	return (NULL);
+}
