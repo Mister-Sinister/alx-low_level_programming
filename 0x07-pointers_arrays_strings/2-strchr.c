3a5951ff35de3862800e32d36c3e56785b74a9a1
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-09-26 15:52:50 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,15 +6,20 @@
  * @c: the character
  * Return: c is the character is present, otherwise NULL
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+		int a;
 
-	return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
